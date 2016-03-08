f4a908ccb8142c725708bedbe850588dff0e0023
Ovidio R. Calvet <calveto@gmail.com>
2016-03-07 20:28:11 -0500

    Added ui-router support to the webapp

--- webapp/app.js
+++ webapp/app.js
@@ -1,17 +1,53 @@
 (function(module) {
 
-  module('webApp', ['ngMaterial'])
-    .controller('AppCtrl', function($http) {
+  module('webApp', ['ngMaterial','ui.router'])
+  .config(function($stateProvider, $urlRouterProvider) {
 
-      var app = this;
+    $urlRouterProvider.otherwise('/contact/list');
 
-      app.title = "Hello World";
-
-      $http.get('http://localhost:8888/contact')
-        .then(function(response) {
-          console.log(response);
-          app.contacts = response.data;
-        })
+    $stateProvider
+    .state('contact', {
+      abstract: true,
+      url: '/contact',
+      template: '<ui-view />'
+    })
+    .state('contact.list', {
+      url: '/list',
+      templateUrl: 'partials/contact-list.html',
+      controller: 'ContactsCtrl as contactsCtrl'
+    })
+    .state('contact.single', {
+      url: '/:contactId',
+      templateUrl: 'partials/contact.html',
+      controller: 'ContactCtrl as contactCtrl'
     });
+
+  })
+  .controller('AppCtrl', function($http) {
+
+    var app = this;
+
+    app.title = "Contact Manager";
+
+  })
+  .controller('ContactsCtrl', function($http, $state) {
+    var contactsCtrl = this;
+
+    $http.get('http://localhost:8888/contact')
+    .then(function(response) {
+      contactsCtrl.contacts = response.data;
+    });
+
+    contactsCtrl.editContact = function(contact) {
+      $state.go('contact.single', { contactId: contact.id });
+    }
+  })
+  .controller('ContactCtrl', function($http, $stateParams) {
+
+    var contactCtrl = this;
+
+    contactCtrl.id = $stateParams.contactId;
+
+  });
 
 })(angular.module)
