9262569b1df8bb7d257bc177a9314258f0cf416c
Ovidio R. Calvet <calveto@gmail.com>
2016-03-08 21:57:37 -0500

    Added  a ionic 2 app and a angular2 client

---
+++ webclient/app/app.js
@@ -0,0 +1,6 @@
+import 'zone.js';
+import 'reflect-metadata';
+import AppComponent from './components/AppComponent.js';
+import { bootstrap } from 'angular2/platform/browser';
+
+bootstrap(AppComponent);
