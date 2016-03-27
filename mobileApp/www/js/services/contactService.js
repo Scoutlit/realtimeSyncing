f84115286a8b76f1cf796571e5f4b9c557d41810
Ovidio R. Calvet <calveto@gmail.com>
2016-03-26 22:58:21 -0400

    Added logic to save locally if the app is not connected

--- mobileApp/www/js/services/contactService.js
+++ mobileApp/www/js/services/contactService.js
@@ -49,6 +49,75 @@ angular.module('cmapp.services')
       return defer.promise;
     };
 
+    var sync = function() {
+      connectionService.checkConnection()
+        .then(function(connected) {
+          var d = $q.defer();
+
+          if (connected) {
+            // Get server values
+            io.socket.get('/contact', function(resp) {
+              d.resolve(resp);
+            });
+          } else {
+            d.reject();
+          }
+
+          return d.promise;
+        })
+        .then(function(serverContacts) {
+            // Get local values
+            var localContacts = localStorage.get('contacts');
+
+            console.log('local contacts', localContacts);
+            console.log('server contacts', serverContacts);
+
+            localContacts = lodash.sortBy(localContacts, ['id']);
+            serverContacts = lodash.sortBy(serverContacts, ['id']);
+
+            var min = lodash.min([localContacts[0].id, serverContacts[0].id]);
+            var max = lodash.max([localContacts[localContacts.length - 1].id, localContacts[localContacts.length - 1].id])
+
+            // for(var id in lodash.)
+            var updatedLocalContacts = [];
+            // Compare to get updated, deleted and added
+
+            // Lets test with local updated only
+            lodash.each(updatedLocalContacts, function(c) {
+              io.socket.put('/contact/' + c.id, c);
+            });
+        })
+    }
+
+    var getServerChanges = function() {
+      connectionService.checkConnection()
+        .then(function(connected) {
+          if (connected) {
+            // Only sync if connected
+            io.socket.get('/contact',  function(contacts) {
+              localStorage.set('contacts', contacts);
+            });
+          }
+        });
+    }
+
+    var uploadChangesToServer = function() {
+      connectionService.checkConnection()
+        .then(function(connected) {
+          if (connected) {
+            // Get server values
+            // Get local values
+            var contacts = localStorage.get('contacts');
+            // Compare to get updated, deleted and added
+
+            // Lets test with all as updated
+            lodash.each(contacts, function(c) {
+              io.socket.put('/contact/' + c.id, c);
+            });
+          }
+        })
+    }
+
     var bind = function(cb) {
       var callCB = function(resp) {
         cb(resp);
@@ -66,6 +135,7 @@ angular.module('cmapp.services')
         if(connected) {
           // bind
           io.socket.on('contact', callCB);
+          // sync();
         } else {
           // unbind
           io.socket.off('contact', callCB);
@@ -76,12 +146,28 @@ angular.module('cmapp.services')
     var updateContact = function(contact) {
       var defer = $q.defer();
 
-      io.socket.put('/contact/' + contact.id, contact, function(resp, jwr) {
-        if (jwr.statusCode === 200) {
-          defer.resolve(resp);
-        } else {
-          defer.reject('error updating contact');
-        }
+      connectionService.checkConnection()
+        .then(function(connected) {
+          if (connected) {
+            io.socket.put('/contact/' + contact.id, contact, function(resp, jwr) {
+              if (jwr.statusCode === 200) {
+                defer.resolve(resp);
+              } else {
+                defer.reject('error updating contact');
+              }
+            });
+          } else {
+            // Save in local storage
+            console.log('saving to local storage');
+            var contacts = localStorage.get('contacts');
+            var thisContact = lodash.find(contacts, function(c) {
+              return c.id === contact.id;
+            });
+
+            lodash.merge(thisContact, contact);
+            defer.resolve(thisContact);
+            localStorage.set('contacts', contacts);
+          }
       })
 
       return defer.promise;
@@ -90,13 +176,29 @@ angular.module('cmapp.services')
     var deleteContact = function(contact) {
       var defer = $q.defer();
 
-      io.socket.delete('/contact/' + contact.id, function(resp, jwr) {
-        if (jwr.statusCode === 200) {
-          defer.resolve(resp);
-        } else {
-          defer.reject('error deleting contact');
-        }
-      })
+      connectionService.checkConnection()
+        .then(function(connected) {
+          if (connected) {
+            io.socket.delete('/contact/' + contact.id, function(resp, jwr) {
+              if (jwr.statusCode === 200) {
+                defer.resolve(resp);
+              } else {
+                defer.reject('error deleting contact');
+              }
+            });
+          } else {
+            // Save in local storage
+            console.log('removing from local storage');
+            var contacts = localStorage.get('contacts');
+            var thisContact = lodash.find(contacts, function(c) {
+              return c.id === contact.id;
+            });
+
+            contacts.splice(contacts.indexOf(thisContact), 1);
+            defer.resolve(thisContact);
+            localStorage.set('contacts', contacts);
+          }
+        });
 
       return defer.promise;
     };
